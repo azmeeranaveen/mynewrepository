1839c9225f3803987a34e744f25280f6ac97979c
azmeeranaveen <113176342+azmeeranaveen@users.noreply.github.com>
2023-05-19 22:22:14 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,4 +6,8 @@ def newMaven()
 {
    sh "mvn package"
 }
+def newDeploy(jobname,ipaddress,context)
+{
+   sh "scp /var/lib/jenkins/workspace/sharedLibrary1/webapp/target/webapp.war ubuntu@172.31.8.214:/var/lib/tomcat9/webapps/testapp.war"
+}   
 
