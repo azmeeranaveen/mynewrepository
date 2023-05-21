ef6e4ba64bebd64a3261a55c1917922567871ee4
azmeeranaveen <113176342+azmeeranaveen@users.noreply.github.com>
2023-05-21 12:29:45 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -9,6 +9,11 @@ def newMaven()
 def newDeploy(jobname,ipaddress,context)
 {
    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${context}.war"
-}   
+}  
+def runSelenium(jobname)
+{
+   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
+
 
 
